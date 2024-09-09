614b05193b967c4d7c60e843b92a115b96846db9
maricarols <maricarolinasilveira@gmail.com>
2024-09-09 11:55:26 -0300

    mudei coisinhas mt fodas

---
+++ questions.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        question: "Você tem problemas com sono?",
+        answers: [
+            { option: "Sim", correct: true },
+            { option: "Não", correct: false },
+        ],
+    },
+    {
+        question: "Dorme mais de 8 horas por dia?",
+        answers: [
+            { option: "Sim", correct: true },
+            { option: "Não", correct: false },
+        ],
+    },
+]
